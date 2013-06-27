97a60ba309dc064c7cb9cff09bf3dbb1c3b098ea
hip2b2 <wyu@ateneo.edu>
2013-06-27 14:45:46 +0800

    updated files to latest version

--- mininet/custom.py
+++ mininet/custom.py
@@ -3,7 +3,7 @@
 # wyu@ateneo.edu
 
 from mininet.net import Mininet
-from mininet.util import createLink
+from mininet.cli import CLI
 net = Mininet()
 
 # Creating nodes in the network.
@@ -12,15 +12,16 @@ h0 = net.addHost('h0')
 s0 = net.addSwitch('s0')
 h1 = net.addHost('h1')
 
-# Creating links between nodes in network (2-ways)
-h0int, s0int = createLink(h0, s0)
+# Creating links between nodes in network
+net.addLink(h0, s0)
 net.addLink(h1, s0)
 
 # Configuration of IP addresses in interfaces
-h0.setIP(h0int, '192.168.1.1', 24)
-h1.setIP(h1int, '192.168.1.2', 24)
+h0.setIP('192.168.1.1', 24)
+h1.setIP('192.168.1.2', 24)
 
 net.start()
 net.pingAll()
+CLI(net)
 net.stop()
 
--- mininet/perf.py
+++ mininet/perf.py
@@ -3,16 +3,23 @@
 # wyu@ateneo.edu
 
 from mininet.net import Mininet
-from mininet.util import createLink
+from mininet.node import CPULimitedHost
+from mininet.link import TCLink
+from mininet.cli import CLI
 
 net = Mininet(host=CPULimitedHost, link=TCLink)
 
+c0 = net.addController()
 s0 = net.addSwitch('s0')
-h0 = net.addHost('h0')
-h1 = net.addHost('h1', cpu=0.5)
+h0 = net.addHost('h0', cpu=0.5)
+h1 = net.addHost('h1')
+h2 = net.addHost('h2')
 
-net.addLink(s0, h0, bw=10, delay='5ms', max_queue_size=1000, loss=10, use_htb=True)
+net.addLink(h0, s0, bw=10, delay='5ms', max_queue_size=1000, loss=10, use_htb=True)
+net.addLink(h1, s0)
+net.addLink(h2, s0)
 
 net.start()
 net.pingAll()
+CLI(net)
 net.stop()
